912f8c6060f74f6beb503c7f9795943a0001483f
Dearc <datingwithme@live.cn>
2016-12-28 11:46:30 +0800

    add tornado WSGI server to run.py

--- run_app.py
+++ run_app.py
@@ -1,6 +1,11 @@
 from app import app
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
 if app.config['CONFIG_NAME'] == 'development':
     app.run(port=5000,threaded=True)
 if app.config['CONFIG_NAME'] == 'production':
-    pass
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000,address='0.0.0.0')
+    IOLoop.instance().start()
