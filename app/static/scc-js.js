bf1d7e574e6be7fc4a025716422148242a319ed3
Dearc <datingwithme@live.cn>
2017-01-16 22:44:15 +0800

    little problem fixed,re-design the comments logic

--- app/static/scc-js.js
+++ app/static/scc-js.js
@@ -2,3 +2,9 @@
  * Created by datin_000 on 2017/1/16.
  */
 //ajax for recent articles/comments/achieve/categories
+var insert_submit_form = function (e) {
+    var _this = $(e);
+    var _this_parent = _this.parents().parents(":first")[0];
+    //must ensure add form only once when clicked many times
+    _this_parent.append('111');
+};
